366e0ee1ff6e7862f64b85e80bd586d42d2c5abc
alpha241412 <alpha.rate99@gmail.com>
2020-10-05 10:04:11 +0700

    crate index php

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <meta charset = "utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+        <title>
+            Latihan Git Remote
+        </title>
+    </head>
+    <body>
+        <h1>Ohayo Sekai</h1>
+    </body>
+</html>
